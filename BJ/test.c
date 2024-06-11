9e555f9ea3719cac4f2087600fec4e779a3e9c87
MINJU <71gnos@naver.com>
2024-06-11 16:28:29 +0900

    TEST

--- BJ/test.c
+++ BJ/test.c
@@ -1,12 +1,22 @@
-#include<cstdio>
+#include <stdio.h>
 
-int a[10001], n, t, i;
+int sum() {
+	static int scount = 5;
+	int acount = 2;
 
-int main() {
-	for (scanf("%d", &n); n--;)
-		scanf("%d", &t), a[t]++;
+	scount++;
+	acount++;
 
-	for (i = 1; i < 10001; i++)
-		while (a[i]--)
-			printf("%d\n", i);
+	return scount + acount;
+}
+
+int main(void) {
+
+	static int scount = 6;
+	int acount = 3;
+
+	printf("sum = %d \t", sum());
+	printf("sum = %d \n", sum());
+
+	return 0;
 }
